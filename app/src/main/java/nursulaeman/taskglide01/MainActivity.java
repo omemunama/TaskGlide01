de224d51f8be14c7c9683462286f51d1653c10ed
omemunama <xnursx@gmail.com>
2016-09-12 18:22:13 +0700

    add ImageView & GLide load

--- app/src/main/java/nursulaeman/taskglide01/MainActivity.java
+++ app/src/main/java/nursulaeman/taskglide01/MainActivity.java
@@ -2,6 +2,9 @@ package nursulaeman.taskglide01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = (ImageView) findViewById(R.id.my_image_view);
+
+        Glide.with(this)
+                .load("http://api.androidhive.info/images/glide/medium/deadpool.jpg")
+                .into(imageView);
+
     }
 }
